c111d86105b514efe0d205d5902f115622d02c06
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-10-27 18:45:43 +0530

    CountFactorprogram

---
+++ CountFactor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfFactors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, x;
+            Console.WriteLine("Enter the Number ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Factors are : ");
+            for (x = 1; x <= num; x++)
+            {
+                if (num % x == 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+
+

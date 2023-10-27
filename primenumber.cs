ab37d941c0de8b1821b4ff533e66d80c0e412235
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-10-27 18:42:29 +0530

    primenumber

---
+++ primenumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Console.Write("Enter a Number : ");
+                int number = int.Parse(Console.ReadLine());
+                bool IsPrime = true;
+                for (int i = 2; i < number / 2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        IsPrime = false;
+                        break;
+                    }
+                }
+                if (IsPrime)
+                {
+                    Console.Write("Number is Prime.");
+                }
+                else
+                {
+                    Console.Write("Number is not Prime.");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+

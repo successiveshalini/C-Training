36c96965f510225a24acf262c8487d639af3c7d2
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-10-27 18:58:40 +0530

    SimpleProgram1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProgram1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, sum = 0, n;
+            Console.Write("Enter the Nth Number : ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 0; i <= n; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("\nSum of N Numbers : " + sum);
+            Console.ReadLine();
+        }
+    }
+}

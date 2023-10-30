9da6a7a6c3c48f5c87ae6acbc6659b516f2b5330
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-10-30 19:38:27 +0530

    RemoveMultipleSpace1

---
+++ Assignment1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveMultipleSpace1
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String : ");
+            string str = Console.ReadLine();
+            str = str.Trim(); 
+            string ans = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ' && str[i + 1] == ' ')
+                {
+                    continue;
+                }
+                else
+                {
+                    ans += str[i];
+                }
+            }
+            Console.WriteLine(ans);
+            Console.ReadKey();
+        }
+        
+    }
+}

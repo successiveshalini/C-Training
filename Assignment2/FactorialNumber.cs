0851035204027c93753b6cdf6e755ea8599a8779
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-07 12:44:26 +0530

    FactorialNumber

---
+++ Assignment2/FactorialNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNumberProgram
+{
+    public class Program
+    {
+        public  int Factorial(int num)
+        {
+            if (num == 0)
+                return 1;
+            else
+            return num * Factorial(num - 1);    
+
+        }
+        static void Main(string[] args)
+        {
+            int Fact, num;
+            Console.WriteLine("Enter a Number");
+            num = Convert.ToInt32(Console.ReadLine()); 
+            Program obj = new Program();
+            Fact = obj.Factorial(num);
+            Console.WriteLine("Factorial of {0} is {1}", num, Fact);
+            Console.ReadKey();
+        }
+    }
+}

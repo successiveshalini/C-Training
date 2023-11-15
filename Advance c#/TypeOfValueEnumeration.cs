9d7c8516f0c6d03dfd603c496c1b62aedb4e2dda
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-15 14:04:52 +0530

    TypeOfValueEnumerationProgram

---
+++ Advance c#/TypeOfValueEnumeration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllowedIntergalTypes
+{
+    public class Program
+    {
+        enum Days : long
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+        static void Main(string[] args)
+        {
+            Type underlinetype = Enum.GetUnderlyingType(typeof(Days));
+            Console.WriteLine("Underlying type of days enum: {UnderlyingType}");
+            Console.WriteLine("Days of the week:");
+            foreach (var day in Enum.GetValues(typeof(Days)))
+            {
+                Console.WriteLine($"{day}: {(long)day}");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}

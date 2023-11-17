aae39a4548b9cd4f28b947048c44e1398efd3356
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-17 16:13:15 +0530

    GenericQueueProgram

---
+++ Advance c#/GenericQueue.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProgram1
+{
+
+    public class Array
+    {
+        public static void Method()
+        {
+            int[] array1 = new int[5];
+            array1[0] = 20;
+            array1[1] = 30;
+            array1[2] = 40;
+            array1[3] = 50;
+            array1[4] = 60;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(array1[i] + " ");
+
+            }
+            Console.WriteLine("\n");
+
+            int[] array2 = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine(array2[i] + " ");
+
+            }
+            Console.WriteLine("\n");
+
+
+            int[,] array2D = new int[2, 3];
+            array2D[0, 0] = 1;
+            array2D[0, 1] = 2;
+            array2D[0, 2] = 3;
+            array2D[1, 0] = 4;
+            array2D[1, 1] = 5;
+            array2D[1, 2] = 6;
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(array2D[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+            int[,] multiDimensionalArray = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(multiDimensionalArray[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+
+
+            int[][] jaggedArray = new int[6][];
+            jaggedArray[0] = new int[4] { 1, 2, 3, 4 };
+            jaggedArray[1] = new int[5] { 84, 42, 90, 11, 56 };
+            jaggedArray[2] = new int[2] { 13, 54 };
+            jaggedArray[3] = new int[1] { 99 };
+            jaggedArray[4] = new int[4] { 19, 22, 93, 42 };
+            jaggedArray[5] = new int[2] { 51, 1 };
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+        public class program
+        {
+            static void Main(string[] args)
+            {
+                //Console.WriteLine();
+                Method();
+                Console.ReadKey();
+
+            }
+
+        }
+
+    }
+}
+    
+
+

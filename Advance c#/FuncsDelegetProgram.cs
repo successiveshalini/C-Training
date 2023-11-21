2e99b721e3fada5f9740f9cadd0f56a0180054ff
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-21 18:59:38 +0530

    FuncDelegetProgram

---
+++ Advance c#/FuncsDelegetProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncDelegetProgram
+{
+    public class Program
+    {
+       public static int Sum(int x, int y, int z)
+        { 
+            return x + y + z;
+
+        }
+        public static int Sub(int x, int y)
+        {
+            return x - y ;
+
+        }
+        static void Main(string[] args)
+        {
+            Func<int, int, int, int> Add = Sum;
+            int result = Add(20, 10, 40);
+            Console.WriteLine(result);  
+            Console.ReadKey();  
+        }
+    }
+}

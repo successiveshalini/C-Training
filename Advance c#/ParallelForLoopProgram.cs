f25e6ff6e36e6f5ad678013844b89e6c0a02d6b7
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-22 17:48:35 +0530

    ParallelForLoopProgram

---
+++ Advance c#/ParallelForLoopProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parallelForLoopProgramming
+{
+ public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# parallel For Loop");
+            Parallel.For(1, 11, number => {
+                Console.WriteLine(number);
+            });
+            Console.ReadKey();  
+        }
+    }
+}

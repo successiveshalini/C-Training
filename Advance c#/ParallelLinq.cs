b00bfa761daebaefdf7a48e95c70b74412130187
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-23 18:20:51 +0530

    ParallelLinqprogram

---
+++ Advance c#/ParallelLinq.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelLinqProgram
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 40);
+            //var evenNumbers = numbers.Where(x => x % 2 == 0).ToList();  
+            
+            var evenNumbers = numbers.AsParallel().Where(x => x % 2 == 0).ToList(); 
+            Console.WriteLine("Even Numbers Between 1 and 40");
+            foreach (var number in evenNumbers)
+            { 
+                Console.WriteLine(number);  
+                Console.ReadKey();  
+
+            }
+            
+            
+        }
+    }
+}
